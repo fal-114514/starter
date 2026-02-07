edceb1b525d064583b24555a6c21695a1f4e7c35
fal-114514 <149204931+fal-114514@users.noreply.github.com>
2026-02-07 17:05:15 +0900

    レイアウトをUSに変更

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -110,10 +110,10 @@ in
         "GroupOrder" = { "0" = "Default"; };
         "Groups/0" = {
           Name = "Default";
-          "Default Layout" = "jp";
+          "Default Layout" = "us";
           DefaultIM = "mozc";
         };
-        "Groups/0/Items/0" = { Name = "keyboard-jp"; };
+        "Groups/0/Items/0" = { Name = "keyboard-us"; };
         "Groups/0/Items/1" = { Name = "mozc"; };
       };
     };
